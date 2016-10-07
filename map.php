557d8d6f322d3ac53c4315e86784c83bc98dcb83
prabhatami456 <prabhat.thakur@effectualtech.com>
2016-10-07 11:53:28 +0530

    First Commit2

--- map.php
+++ map.php
@@ -1,4 +1,4 @@
-<!--<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.it/maps?q=delhi&output=embed"></iframe>-->
+<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.it/maps?q=delhi&output=embed"></iframe>
 
 <?php
 $address="KVR SCO-5";
